38c628c6156927c6b51c4c549b3de5d3d3ff7ae1
RLeclair <raulleclair@microsoft.com>
2022-09-20 15:25:36 -0700

    Adding gyroscope, humidity, barometer

--- STMicroelectronics/B-L475E-IOT01A/app/sensors.c
+++ STMicroelectronics/B-L475E-IOT01A/app/sensors.c
@@ -4,9 +4,14 @@
 #include "services/jd_services.h"
 #include "stm32l475e_iot01_tsensor.h"
 #include "stm32l475e_iot01_accelero.h"
+#include "stm32l475e_iot01_gyro.h"
+#include "stm32l475e_iot01_hsensor.h"
+#include "stm32l475e_iot01_psensor.h"
 #include <math.h>
 
 static env_reading_t temp_r = {0, 512, -40 * 1024, 125 * 1024};
+static env_reading_t humi_r = {0, 3584, 0, 100 * 1024};
+static env_reading_t baro_r = {0, 103, 260 * 1024, 1100 * 1024};
 
 void accelerometer_data_transform(int32_t sample[3]) 
 { 
@@ -20,11 +25,24 @@ void accelerometer_data_transform(int32_t sample[3])
 
 static void void_sensor_func(void) { }
 
-static void* l475_temperature(void)
+static void* l475_get_temperature(void)
 {
     temp_r.value = (int32_t) round(BSP_TSENSOR_ReadTemp() * 1024);
     return &temp_r;
 }
+
+static void* l475_get_humidity(void)
+{
+    humi_r.value = (int32_t) round(BSP_HSENSOR_ReadHumidity() * 1024);
+    return &humi_r;
+}
+
+static void* l475_get_barometer(void)
+{
+    baro_r.value = (int32_t) round(BSP_PSENSOR_ReadPressure() * 1024 * 100);
+    return &baro_r;
+}
+
 
 static void* l475_get_accelerometer(void)
 {
@@ -39,11 +57,36 @@ static void* l475_get_accelerometer(void)
     return sample;
 }
 
+static void* l475_get_gyroscope(void)
+{
+    float data[3];
+    static int32_t sample[3];
+    BSP_GYRO_GetXYZ(data);
+
+    sample[0] = ((int32_t)round(data[0])) * 1024;
+    sample[1] = ((int32_t)round(data[1])) * 1024;
+    sample[2] = ((int32_t)round(data[2])) * 1024;
+
+    return sample;
+}
 
 const env_sensor_api_t temperature_l475 = {
     .init = void_sensor_func, 
     .process = void_sensor_func, 
-    .get_reading = l475_temperature
+    .get_reading = l475_get_temperature
+};
+
+const env_sensor_api_t l475_humidity = {
+    .init = void_sensor_func,
+    .process = void_sensor_func,
+    .get_reading = l475_get_humidity
+
+};
+
+const env_sensor_api_t l475_barometer = {
+    .init = void_sensor_func,
+    .process = void_sensor_func,
+    .get_reading = l475_get_barometer
 };
 
 const accelerometer_api_t l475_accelerometer = {
@@ -51,15 +94,18 @@ const accelerometer_api_t l475_accelerometer = {
     .get_reading = l475_get_accelerometer,
     .sleep       = void_sensor_func
 };
-/*
+
 const gyroscope_api_t l475_gyroscope = {
     .init = void_sensor_func, 
-    .get_reading = l475_get_accelerometer, 
+    .get_reading = l475_get_gyroscope, 
     .sleep = void_sensor_func
 };
-*/
+
 void init_sensors(void)
 {
     temperature_init(&temperature_l475);
     accelerometer_init(&l475_accelerometer);
+    gyroscope_init(&l475_gyroscope);
+    humidity_init(&l475_humidity);
+    barometer_init(&l475_barometer);
 }
