2453585584e222f0d6a1e76dac51109b7343f66d
Michal Moskal <michal@moskal.me>
2022-09-20 11:35:07 -0700

    enable usb

--- STMicroelectronics/B-L475E-IOT01A/app/usbd_desc.c
+++ STMicroelectronics/B-L475E-IOT01A/app/usbd_desc.c
@@ -23,8 +23,13 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-#define USBD_VID                      0x0483
-#define USBD_PID                      0x5740
+
+
+
+#define USBD_VID                      0x2E8A // RPI
+#define USBD_PID                      0x9fd2 // random
+//#define USBD_VID                      0x0483
+//#define USBD_PID                      0x5740
 #define USBD_LANGID_STRING            0x409
 #define USBD_MANUFACTURER_STRING      "STMicroelectronics"
 #define USBD_PRODUCT_FS_STRING        "Jacdac Serial"
